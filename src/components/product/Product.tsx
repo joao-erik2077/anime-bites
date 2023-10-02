3544ca6edbf3927b94cbb55c308b2d8d49ed8402
joao-erik2077 <joaoerik2074@gmail.com>
2023-10-02 08:29:25 -0300

    product stylization

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -13,7 +13,7 @@ export default function Product({name, img, price}: ProductProps) {
                 <img src={img} alt={name} className={styles.product__image_container__image} />
             </div>
             <div className={styles.product__info}>
-                <h1 className={styles.product__info__name}>{ name }</h1>
+                <p className={styles.product__info__name}>{ name }</p>
                 <p className={styles.product__info__price}>{ price }</p>
             </div>
         </div>
