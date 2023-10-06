8d5fe306a8977629097687bb414b8a06265d39fe
joao-erik2077 <joaoerik2074@gmail.com>
2023-10-06 18:00:13 -0300

    basic footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar/Navbar';
 import Main from './pages/main/Main';
 import Products from './pages/products/Products';
+import Footer from './components/footer/Footer';
 
 export default function App() {
   return <>
     <Navbar />
     <Main />
     <Products />
+    <Footer />
   </>;
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footbar.module.scss';
+
+export default function Footer() {
+    return <>
+        <footer className={styles.footer}>
+
+        </footer>
+    </>;
+}
